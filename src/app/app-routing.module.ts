3cda6dd49fffd15b31042d8c64ae04ec39bb19a5
edbuleo <edbuleo@ESPC-GRM-EDBULE>
2022-04-21 15:02:21 +0200

    cambio inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LocalComponent } from './componentes/estaticos/local/local.component';
+import { InicioComponent } from './componentes/estaticos/inicio/inicio.component';
 
-import { InicioSesionComponent } from './componentes/estaticos/inicio-sesion/inicio-sesion.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'home', component: HomeComponent }, 
   { path: 'local/:id_local', component: LocalComponent  },
-  { path: 'inicio', component:InicioSesionComponent   },
+   { path: 'inicio', component: InicioComponent   },
   { path: '**', pathMatch:'full', redirectTo:'home' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
-import { InicioSesionComponent } from './componentes/estaticos/inicio-sesion/inicio-sesion.component';
-import { InicioComponent } from './componentes/estaticos/inicio-sesion/inicio/inicio.component';
+import { InicioComponent } from './componentes/estaticos/inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
@@ -31,9 +31,9 @@ import { InicioComponent } from './componentes/estaticos/inicio-sesion/inicio/in
     LocalComponent,
     LoadingComponent,
     CarruselComponent,
+    InicioComponent,
   
-    InicioSesionComponent,
-    InicioComponent, 
+
     
     
   ],

917a1d3232df55f4c552960ed38ec0cc743e39bc
edbuleo <edbuleo@ESPC-GRM-EDBULE>
2022-05-02 17:19:37 +0200

    add url server en pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true, 
-  urlServer : "http://dws.local/api",
+  //urlServer : "http://dws.local/api",
+  urlServer : "https://eduardobuleodaw.000webhostapp.com/api",
+
+  
   userCode : "4143bfc08b90fb34d0714382a64f4f6e"
 };
